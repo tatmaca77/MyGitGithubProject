d0445c6482c44ac8d28ff45a134a874bff8ae689
Tolgahan <tolgahanatmaca49@gmail.com>
2023-04-26 22:02:31 +0200

    tlghn2

---
+++ src/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+
+    public static void main(String[] args) {
+
+        System.out.println("bazi kodlar yazdim");
+        // version tlghn1
+
+        System.out.println("bir kisim kodlar");
+        //tlghn2
+    }
+}
